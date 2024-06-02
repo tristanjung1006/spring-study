5ca905ddabed68a0cc2080d7c447fd1cf0beedfc
Daemon Jung <jjjssswww1006@gmail.com>
2024-06-03 04:19:49 +0900

    Add AOP

---
+++ src/main/java/com/example/demo/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package com.example.demo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    // 공통 관심사를 올바르게 타겟팅
+    @Around("execution(* com.example.demo..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs +
+                    "ms");
+        }
+    }
+}
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -3,12 +3,13 @@ package com.example.demo.service;
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Transactional
 public class MemberService {
 
     // MemberService의 테스트 코드르 작성하려면 Ctrl + Shift + T로 틀을 빠르게 만들 수 있다.
@@ -25,12 +26,17 @@ public class MemberService {
         // 같은 이름이 있는 회원은 안된다
         // 과거에는 if문으로 null의 여부를 판단하여 진행했지만 Optional을 사용해서 다양한 메소드를 사용할 수 있다
 
-        validateDuplicateMember(member);
-        // 위에서 메소드로 뽑는 것이 낫다.
+        long start = System.currentTimeMillis();
 
-
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -45,8 +51,16 @@ public class MemberService {
      */
     // 서비스는 기획자와 협업을 원활히 하기 위해서 비즈니스에 의존적으로 네이밍 및 설계를 하고
     // 리포지토리 같은 경우에는 개발스러운 용어를 선택한다.
+
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
